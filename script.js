8df071b1272addda6e3f91711df9f27e6f509440
lecram <15247414+LeCram0ni@users.noreply.github.com>
2024-08-05 16:11:33 +0200

    update0508

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
+let first = true;
+
 function start(){
 
     display.innerHTML = `<div id="text">0</div>`;
-
+    first = true;
     //console.log("Test");
 }
 
-function update(){
-    document.getElementById('text').innerHTML += 'test';
-    
+function clear(){
+    document.getElementById('text').innerHTML = "0";
+    console.log("clear");
+}
+
+function update(button){
+    if (first){
+        clear();
+    }
+    first = false;
+    document.getElementById('text').innerHTML += button;
     console.log("test");
+}
+
+function calculate(){
+    let toCalc = document.getElementById("text").innerHTML;
+    console.log(toCalc);
+    let calculated = eval(toCalc);
+    console.log(calculated);
 }
